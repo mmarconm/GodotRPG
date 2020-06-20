376d6993355511b9a16d66b7d57ff4d4e84ebdb8
Marcelo Marcon <marcelomarcon@outlook.com>
2020-06-20 12:14:27 -0400

    animationTree added

--- Player.gd
+++ Player.gd
@@ -6,8 +6,10 @@ const MAX_SPEED = 80
 const FRICTION = 500
 
 # Variables
+# usando $ voce consegue ter acesso a todoso os nodes
 onready var animationPlayer = $AnimationPlayer
-var velocity = Vector2.ZERO
+onready var animationTree = $AnimationTree
+var velocity = Vector2.ZERO # agora é um vector 2 com (0,0)
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
@@ -19,14 +21,8 @@ func _physics_process(delta: float) -> void:
 	input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		elif input_vector.x < 0:
-			animationPlayer.play("RunLeft")
-		elif input_vector.y > 0:
-			animationPlayer.play("RunDown")
-		elif input_vector.y < 0:
-			animationPlayer.play("RunUp")
+		animationTree.set('parameters/Idle/blend_position', input_vector)
+		animationTree.set('parameters/Run/blend_position', input_vector)
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		# print(velocity)
 	else:
